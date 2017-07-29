e6f7701e4c6f03387b0ffbe13216fee6b7bc0b37
nihaocun <nihaocun@163.com>
2017-07-29 16:08:38 +0800

    蛤

---
+++ 360f4复活大法/app/src/main/java/tros/runanjing/qhmp/zjm.java
@@ -0,0 +1,17 @@
+package tros.runanjing.qhmp;
+
+import android.app.*;
+import android.content.*;
+import android.os.*;
+import android.view.*;
+
+public class zjm extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.zjm);
+		
+    }
+}
